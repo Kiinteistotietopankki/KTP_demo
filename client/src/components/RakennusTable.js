d9333dfca86b8d8b4192fd816c84c6c475b7f898
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2025-04-07 12:36:46 +0300

    smarter temporary storing in json

--- client/src/components/RakennusTable.js
+++ client/src/components/RakennusTable.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Tabletemplate from './Tabletemplate';
-
+import { Accordion } from 'react-bootstrap';
 
 function Rakennustable({ data }) {
     const [rakennukset, setRakennukset] = useState([]);
@@ -16,7 +16,7 @@ function Rakennustable({ data }) {
                     coordinates: feature.geometry.coordinates
                 },
                 properties: {
-                    basicInformation: {
+                    yleistiedot: {
                         "Rakennustunnus": feature.properties.permanent_building_identifier || null,
                         "Kiinteistötunnus": feature.properties.property_identifier || null,
                         "Kohteen nimi": null,
@@ -24,7 +24,7 @@ function Rakennustable({ data }) {
                         "Postinumero" : null, 
                         "Toimipaikka": null,
                     },
-                    buildingSpecifications: {
+                    teknisettiedot: {
                         "Rakennusvuosi": feature.properties.completion_date ? feature.properties.completion_date.split("-")[0] : null,
                         "Kokonaisala (m²)": feature.properties.total_area || null,
                         "Kerrosala (m²)": feature.properties.gross_floor_area || null,
@@ -32,16 +32,16 @@ function Rakennustable({ data }) {
                         "Tilavuus (m³)": feature.properties.volume || null,
                         "Kerroksia": feature.properties.number_of_storeys || null,
                     },
-                    constructionDetails: {
+                    rakennustiedot: {
                         "Rakennusluokitus": feature.properties.main_purpose || null,
                         "Runkotapa": feature.properties.construction_method || null,
                         "Käytössäolotilanne": feature.properties.usage_status || null,
-                        "Julkisivunrakennusaine": feature.properties.facade_material || null,
+                        "Julkisivun rakennusaine": feature.properties.facade_material || null,
                         "Lämmitystapa": feature.properties.heating_method || null,
                         "Lämmitysenergianlähde": feature.properties.heating_energy_source || null,
                         "Kantavanrakenteen rakennusaine": feature.properties.material_of_load_bearing_structures || null,
                     },
-                    environmentalInformation: {
+                    aluetiedot: {
                         "Tulvariski": null, 
                         "Pohjavesialueella": null
                     }
@@ -57,13 +57,57 @@ function Rakennustable({ data }) {
             {rakennukset.length > 0 ? (
                 <>
                     {rakennukset.map((rakennus, index) => (
-                        <div key={index}>
+                        <div key={index} className="card mb-4">
                             {/* Pass the entire basicInformation object */}
-                            <Tabletemplate 
-                                properties={rakennus.properties.basicInformation} 
-                                tableTitle={''} 
-                            />
+                            <div className="card-header">Rakennus {rakennus.properties.yleistiedot.Rakennustunnus}</div>
+
+                                <Accordion defaultActiveKey={['0']} alwaysOpen='true'>
+                                    <Accordion.Item eventKey="0">
+                                        <Accordion.Header>Yleistiedot</Accordion.Header>
+                                        <Accordion.Body>
+                                            <Tabletemplate 
+                                                properties={rakennus.properties.yleistiedot} 
+                                                tableTitle={''} 
+                                            />
+                                        </Accordion.Body>
+                                    </Accordion.Item>
+
+                                    <Accordion.Item eventKey="1">
+                                        <Accordion.Header>Tekniset tiedot</Accordion.Header>
+                                            <Accordion.Body>
+                                            <Tabletemplate 
+                                                properties={rakennus.properties.teknisettiedot} 
+                                                tableTitle={''} 
+                                            />
+                                            </Accordion.Body>
+                                    </Accordion.Item>
+
+                                    <Accordion.Item eventKey="2">
+                                        <Accordion.Header>Rakennustiedot</Accordion.Header>
+                                            <Accordion.Body>
+                                            <Tabletemplate 
+                                                properties={rakennus.properties.rakennustiedot} 
+                                                tableTitle={''} 
+                                            />
+                                            </Accordion.Body>
+                                    </Accordion.Item>
+
+                                    <Accordion.Item eventKey="3">
+                                        <Accordion.Header>Aluetiedot</Accordion.Header>
+                                            <Accordion.Body>
+                                            <Tabletemplate 
+                                                properties={rakennus.properties.aluetiedot} 
+                                                tableTitle={''} 
+                                            />
+                                            </Accordion.Body>
+                                    </Accordion.Item>
+                                </Accordion>
+                            
+
                         </div>
+
+                    
+
                     ))}
                 </>
             ) : (
