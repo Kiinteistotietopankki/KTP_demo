f1af3d797c33c45a3982262abf4dd6a52d626fce
Henri Hyvärinen <Henri.hyvarinen1@gmail.com>
2025-04-09 15:56:55 +0300

    Update Config.js

--- client/src/Config.js
+++ client/src/Config.js
@@ -1,6 +1,6 @@
 export const config = {
   appId: '6ac8e653-12ea-426a-b3b0-dc1078e34bb2', //(client) ID
-  redirectUri: 'http://localhost:3000/', 
+  redirectUri: 'https://ktp-demo.onrender.com/', 
   scopes: ['user.read'],
   authority: 'https://login.microsoftonline.com/waativa.fi',
 };
