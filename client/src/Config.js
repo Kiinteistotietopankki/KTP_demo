52e49304ffdd59ed217d973f49432fb524f36c3b
Henri Hyvärinen <Henri.hyvarinen1@gmail.com>
2025-04-09 17:24:45 +0300

    config pois

--- client/src/Config.js
+++ client/src/Config.js
@@ -1,7 +1,7 @@
-export const config = {
+/*export const config = {
   appId: '6ac8e653-12ea-426a-b3b0-dc1078e34bb2', //(client) ID
   redirectUri: 'https://ktp-demo.onrender.com/', 
   scopes: ['user.read'],
   authority: 'https://login.microsoftonline.com/waativa.fi',
 };
-
+*/
