12ee0f916cf61519fa7e3f60fcfe1e12200cde21
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2025-04-14 12:36:32 +0300

    still works

--- client/src/components/MapComponent.js
+++ client/src/components/MapComponent.js
@@ -1,42 +1,43 @@
-import React from 'react';
-import { MapContainer, WMSTileLayer } from "react-leaflet";
-import L, { vectorGrid } from "leaflet";
-import "leaflet/dist/leaflet.css";
-
-
-
-
+import React, { useEffect, useRef } from 'react';
+import L from 'leaflet'; // Import Leaflet
+import { CRS } from 'leaflet';
+import 'leaflet/dist/leaflet.css'; // Import Leaflet's CSS¨
 
 const MapComponent = () => {
+  const mapRef = useRef(null);
 
-  const username = "Waativakarttakoe";
-  const password = "Karttamiesukko12";
-  
-  // Base64 encode the credentials
-  const base64Credentials = btoa(`${username}:${password}`);
+  const projectedPoint = L.point(2834228.3862621, 9610523.68631807); 
+  const latlng = L.CRS.EPSG3857.unproject(projectedPoint); // Converts to [lat, lng]
 
 
-  // wms?service=WMS&version=1.3.0&request=GetMap&layers=public:taustakartta&SRSNAME=EPSG:4326
-  const wmsUrl = "https://sopimus-karttakuva.maanmittauslaitos.fi/sopimus/service/wms?SRSNAME=EPSG:4326"
-  
-  return (
-    <div>
-      <MapContainer 
-        center={[60.3219, 24.7481]} 
-        zoom={8} 
-        style={{ width: '100%', height: '100%' }}
-      >
+  useEffect(() => {
+    // Create the map and set the initial view
+    const map = L.map(mapRef.current).setView(
+      [64.91089078, 25.52603082]
+      , 13);  // Initial center and zoom level
 
-        <WMSTileLayer
-          url=''
-          layers='public:taustakartta'
-        ></WMSTileLayer>
-        
+    // Add the OpenStreetMap tile layer
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
 
+    // Add the WMS layer
+    const wmsLayer = L.tileLayer.wms('https://sopimus-karttakuva.maanmittauslaitos.fi/sopimus/service/wms?SRSNAME=EPSG:4326&', {
+      layers: 'public:peruskartta_taustavari',
+      headers: {
+        'Authorization': 'Basic ' + btoa('Waativakarttakoe:Karttamiesukko12') // Replace with your actual username and password
+      }
+    }).addTo(map);
 
-      </MapContainer>
-    </div>
-  );
+    // Clean up when the component is unmounted
+    return () => {
+      map.removeLayer(wmsLayer);
+      map.remove();
+    };
+  }, []);
+
+  return <div id="map" ref={mapRef} style={{ height: '500px', width: '100%' }}></div>;
 };
 
 export default MapComponent;
