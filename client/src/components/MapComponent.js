b370492f6da274162cadeccbd1387f0ae0a9a7b4
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2025-04-14 17:13:15 +0300

    Kartta ja layereita

--- client/src/components/MapComponent.js
+++ client/src/components/MapComponent.js
@@ -5,63 +5,63 @@ import 'leaflet-wms-header';
 import 'leaflet/dist/leaflet.css'; // Import Leaflet's CSS¨
 
 
-// const MapComponent = () => {
-//   const mapRef = useRef(null);
+const MapComponent = () => {
+  const mapRef = useRef(null);
 
-//   const projectedPoint = L.point(2834228.3862621, 9610523.68631807); 
+  const projectedPoint = L.point(2834228.3862621, 9610523.68631807); 
 
-//   const username='Waativakarttakoe'
-//   const password='Karttamiesukko12'
+  const username='Waativakarttakoe'
+  const password='Karttamiesukko12'
 
 
-//   useEffect(() => {
-//     // Create the map and set the initial view
-//     const map = L.map(mapRef.current, {
-//       center: [64.91089078, 25.52603082], // Starting position
-//       zoom: 13, // Zoom level
-//       // scrollWheelZoom: false,
-//     });
+  useEffect(() => {
+    // Create the map and set the initial view
+    const map = L.map(mapRef.current, {
+      center: [64.91089078, 25.52603082], // Starting position
+      zoom: 13, // Zoom level
+      // scrollWheelZoom: false,
+    });
 
-//     // Add the OpenStreetMap tile layer
-//     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//       attribution:
-//         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-//     }).addTo(map);
+    // Add the OpenStreetMap tile layer
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
 
-//     // Add the WMS layer
-//     // const wmsLayer = L.TileLayer.wms('https://sopimus-karttakuva.maanmittauslaitos.fi/sopimus/service/wms?SRSNAME=EPSG:4326&', {
-//     //   layers: 'public:peruskartta_taustavari',
-//     //   headers: {
-//     //     'Authorization': 'Basic ' + btoa('Waativakarttakoe:Karttamiesukko12') // Replace with your actual username and password
-//     //   }
-//     // }).addTo(map);
+    Add the WMS layer
+    const wmsLayer = L.TileLayer.wms('https://sopimus-karttakuva.maanmittauslaitos.fi/sopimus/service/wms?SRSNAME=EPSG:4326&', {
+      layers: 'public:peruskartta_taustavari',
+      headers: {
+        'Authorization': 'Basic ' + btoa('Waativakarttakoe:Karttamiesukko12') // Replace with your actual username and password
+      }
+    }).addTo(map);
 
-//   //   var wmsLayer = L.tileLayer.wmsHeader(
-//   //     'https://sopimus-karttakuva.maanmittauslaitos.fi/sopimus/service/wms?',
-//   //     {
-//   //         layers: 'public:taustakartta',
-//   //         format: 'image/png',
-//   //         transparent: true,
-//   //     },
-//   //     [
-//   //       { 
-//   //           header: 'Authorization', 
-//   //           value: `Basic ${btoa(username + ':' + password)}` 
-//   //       },
-//   //     ],
-//   //     null
-//   // ).addTo(map);
+    var wmsLayer = L.tileLayer.wmsHeader(
+      'https://sopimus-karttakuva.maanmittauslaitos.fi/sopimus/service/wms?',
+      {
+          layers: 'public:taustakartta',
+          format: 'image/png',
+          transparent: true,
+      },
+      [
+        { 
+            header: 'Authorization', 
+            value: `Basic ${btoa(username + ':' + password)}` 
+        },
+      ],
+      null
+  ).addTo(map);
 
-//     // Clean up when the component is unmounted
-//     return () => {
-//       // map.removeLayer(wmsLayer);
-//       map.remove();
-//     };
-//   }, []);
+    // Clean up when the component is unmounted
+    return () => {
+      // map.removeLayer(wmsLayer);
+      map.remove();
+    };
+  }, []);
 
-//   return (
-//     <div id="map" ref={mapRef} style={{ height: '500px', width: '100%' }}></div>
-//   );
-// };
+  return (
+    <div id="map" ref={mapRef} style={{ height: '500px', width: '100%' }}></div>
+  );
+};
 
-// export default MapComponent;
+export default MapComponent;
--- client/src/components/MapVisual.js
+++ client/src/components/MapVisual.js
@@ -1,47 +1,100 @@
 import React, { useEffect, useRef } from 'react';
-import L from 'leaflet'; // Import Leaflet
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
-import 'leaflet/dist/leaflet.css'; // Import Leaflet's CSS¨
-const apiKey = process.env.API_KEY;  // Access the API key from the environment variables
-  
+
+const apiKey = process.env.API_KEY;
+
 const MapVisual = () => {
-    const mapRef = useRef(null);
-  
-    useEffect(() => {
-      // Create the map and set the initial view
-      var apiKey = 'f8a1b3f2-6fce-4dc8-a560-b59c7cc446bb';
+  const mapRef = useRef(null);
 
-      var map = L.map('map', {
-        center: [64.91089078, 25.52603082],
-        zoom: 17,
-        minZoom: 16,
-        maxZoom: 18,
-        dragging: false,
-        scrollWheelZoom: false // Disables zooming with mouse wheel
-      })
-      
-      var template =
-      `https://avoin-karttakuva.maanmittauslaitos.fi/avoin/wmts/1.0.0/taustakartta/default/{tileMatrixSet}/{z}/{y}/{x}.png?apiKey=${apiKey}`;
-      
-      var layer = L.tileLayer(template, {
-        tileMatrixSet: 'WGS84_Pseudo-Mercator'
-      }).addTo(map);
+  const position = [64.91089078, 25.52603082]
+
+  useEffect(() => {
+    const map = L.map('map', {
+      center: position,
+      zoom: 16,
+      minZoom: 15,
+      maxZoom: 17,
+      dragging: false,
+      scrollWheelZoom: false
+    });
+
+    L.Icon.Default.mergeOptions({
+        iconUrl: require('leaflet/dist/images/marker-icon.png'),
+        iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+        shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+      });
+
+    const template = `https://avoin-karttakuva.maanmittauslaitos.fi/avoin/wmts/1.0.0/{layerName}/default/{tileMatrixSet}/{z}/{y}/{x}.png?apiKey=${apiKey}`;
+
+    // Layers
+    const layerBaseMap = L.tileLayer(template, {
+      tileMatrixSet: 'WGS84_Pseudo-Mercator',
+      layerName: 'taustakartta',
+      attribution: '&copy; Maanmittauslaitos',
+      detectRetina: true
+    });
+
+    const layerBaseMapIlmakuva = L.tileLayer(template, {
+        tileMatrixSet: 'WGS84_Pseudo-Mercator',
+        layerName: 'ortokuva',
+        attribution: '&copy; Maanmittauslaitos',
+        detectRetina: true
+    });
+
+    const layerBaseMapSelko = L.tileLayer(template, {
+        tileMatrixSet: 'WGS84_Pseudo-Mercator',
+        layerName: 'selkokartta',
+        attribution: '&copy; Maanmittauslaitos',
+        detectRetina: true
+    });
+
+    const layerKiinteistotunnus = L.tileLayer(template, {
+      tileMatrixSet: 'WGS84_Pseudo-Mercator',
+      layerName: 'kiinteistotunnukset',
+      detectRetina: true
+    });
+
+    const layerKiinteistorajat = L.tileLayer(template, {
+      tileMatrixSet: 'WGS84_Pseudo-Mercator',
+      layerName: 'kiinteistojaotus',
+      detectRetina: true
+    });
+
+    // Add default layer
+    layerBaseMap.addTo(map);
+
+    // Create layer control
+    const baseMaps = {
+      'Taustakartta': layerBaseMap,
+      'Ilmakuva': layerBaseMapIlmakuva,
+      'Selkokartta': layerBaseMapSelko
+    };
+
+    const overlayMaps = {
+      'Kiinteistötunnukset': layerKiinteistotunnus,
+      'Kiinteistörajat': layerKiinteistorajat
+    };
+
+    L.control.layers(baseMaps, overlayMaps, { collapsed: false }).addTo(map);
 
 
-  
-      // Clean up when the component is unmounted
-      return () => {
-        map.removeLayer(layer);
-        map.remove();
-      };
-    }, []);
-  
-    return (
-        <div className='mapcont mt-5 mb-5'>
-            <div id="map" ref={mapRef} style={{ height: '300px', maxWidth: '70%'}}></div>
-        </div>
-    );
-  };
-  
-  
-  export default MapVisual;
+    const marker = L.marker(position).addTo(map);
+    marker.bindPopup('Default Icon Popup');
+
+    // Clean up
+    return () => {
+      map.remove();
+    };
+  }, []);
+
+  return (
+    <div className='mapcont mt-5 mb-5'>
+      <div id="map" ref={mapRef} style={{ height: '500px', maxWidth: '100%' }}>
+      </div>
+    </div>
+  );
+};
+
+export default MapVisual;
