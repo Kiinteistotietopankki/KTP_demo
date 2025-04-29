7f8563c3c6329f649391c7b8c326966b9dd5b7f4
Henri Hyvärinen <Henri.hyvarinen1@gmail.com>
2025-04-29 10:30:35 +0300

    asadf
    tetet

--- client/src/components/Resultsdisplay.js
+++ client/src/components/Resultsdisplay.js
@@ -2,18 +2,13 @@ import React, { useEffect, useState } from 'react';
 import Tabletemplate from './Tabletemplate';
 import { Accordion } from 'react-bootstrap';
 import exportToExcel from './Excelexport';
-import exportToPdf from './Pdfexport';
 import EditableReport from './ReportTemplate';
+import exportToPdf from './Pdfexport';
 import Modal from 'react-bootstrap/Modal';
 import '../App.css';
 
-function Resultdisplay({ data, setMapCoords }) {
-  const [kiinteistot, setKiinteistot] = useState([]);
-  const [selectedRakennukset, setSelectedRakennukset] = useState({});
-  const [showReport, setShowReport] = useState(false);
-  const [reportRakennus, setReportRakennus] = useState(null);
-
-
+function Resultdisplay({ data, setMapCoords}) {
+    const [kiinteistot, setKiinteistot] = useState([]);
 
     useEffect(() => {
         if (data.length > 0){
@@ -93,7 +88,7 @@ function Resultdisplay({ data, setMapCoords }) {
     <div className="mt-4">
       {kiinteistot.map((kiinteisto, kiinteistoIndex) => (
         <div key={kiinteistoIndex} className="kiinteistocard card mb-4 p-2 border border-primary bg-dark text-white p-1">
-          <div className="card-header d-flex gap-3 align-items-center ms-3">
+          <div className="card-header d-flex gap-3 align-items-center ms-2">
             <p className="h4">Kiinteistö <span className='text-decoration-underline'>{kiinteisto?.id_esitysmuoto_kiinteistotunnus || "N/A"}</span></p>
             <button className="btn btn-outline-secondary btn-sm d-flex align-items-center" onClick={() => copyText(kiinteisto?.id_esitysmuoto_kiinteistotunnus)}>
               <i className="bi bi-clipboard me-2"></i> Kopioi
@@ -103,17 +98,17 @@ function Resultdisplay({ data, setMapCoords }) {
           {kiinteisto.rakennukset?.length > 0 ? (
             <>
               <div className="d-flex justify-content-between align-items-center p-2">
-                <label className="d-flex align-items-center gap-2">
-                <input
-                     type="checkbox"
-                checked={
-                kiinteisto.rakennukset.every(rakennus => selectedRakennukset[rakennus.properties.yleistiedot.Rakennustunnus?.value])
-                    }
-                     onChange={handleSelectAll}
-                    />
+                <label className="d-flex align-items-center gap-2 ms-4">
+                    <input
+                        type="checkbox"
+                    checked={
+                    kiinteisto.rakennukset.every(rakennus => selectedRakennukset[rakennus.properties.yleistiedot.Rakennustunnus?.value])
+                        }
+                        onChange={handleSelectAll}
+                        />
 
-                  
-                  Valitse kaikki rakennukset
+                    
+                    Valitse kaikki rakennukset
                 </label>
                 <div className="d-flex gap-2">
                   <button className="export-button" onClick={handleCreateReport}>Luo raportti</button>
@@ -144,54 +139,92 @@ function Resultdisplay({ data, setMapCoords }) {
                       >
                         <i className="bi bi-geo-alt-fill me-1"></i> Näytä kartalla
                       </button>
+    return (
+        <div className="mt-4">
+            {kiinteistot.map((kiinteisto, kiinteistoIndex) => (
+                <div key={kiinteistoIndex} className="kiinteistocard card mb-4 p-2 border border-primary bg-dark text-white p-1">
+                    <div className="card-header d-flex gap-3 align-items-center ms-3">
+                        <p className="h4">Kiinteistö <span className='text-decoration-underline'>{kiinteisto?.id_esitysmuoto_kiinteistotunnus || "N/A"} </span></p>
+                        <button className="btn btn-outline-secondary btn-sm d-flex align-items-center" onClick={() => copyText(kiinteisto?.id_esitysmuoto_kiinteistotunnus)}>
+                            <i className="bi bi-clipboard me-2"></i> Kopioi
+                        </button>
                     </div>
 
-                    <Accordion>
-                      <Accordion.Item eventKey="0">
-                        <Accordion.Header>Yleistiedot</Accordion.Header>
-                        <Accordion.Body>
-                          <Tabletemplate properties={rakennus.properties.yleistiedot} />
-                        </Accordion.Body>
-                      </Accordion.Item>
-                      <Accordion.Item eventKey="1">
-                        <Accordion.Header>Tekniset tiedot</Accordion.Header>
-                        <Accordion.Body>
-                          <Tabletemplate properties={rakennus.properties.teknisettiedot} />
-                        </Accordion.Body>
-                      </Accordion.Item>
-                      <Accordion.Item eventKey="2">
-                        <Accordion.Header>Rakennustiedot</Accordion.Header>
-                        <Accordion.Body>
-                          <Tabletemplate properties={rakennus.properties.rakennustiedot} />
-                        </Accordion.Body>
-                      </Accordion.Item>
-                      <Accordion.Item eventKey="3">
-                        <Accordion.Header>Aluetiedot</Accordion.Header>
-                        <Accordion.Body>
-                          <Tabletemplate properties={rakennus.properties.aluetiedot} />
-                        </Accordion.Body>
-                      </Accordion.Item>
-                    </Accordion>
-                  </div>
-                </div>
-              ))}
-            </>
-          ) : (
-            <div className="p-2">Ei rakennuksia</div>
-          )}
-        </div>
-      ))}
+                    {kiinteisto.rakennukset?.length > 0 ? (
+                        kiinteisto.rakennukset.map((rakennus, rakennusIndex) => (
+                            <div key={rakennusIndex} className="card mb-4 p-1">
+                                <div className="card-body">
+                                    <div className="card-header d-flex justify-content-between align-items-center">
+                                    <span>
+                                        Rakennus {rakennus.id_rakennustunnus}{" "}
+                                        {Array.isArray(rakennus.properties?.yleistiedot["Kohteen osoitteet"].value)
+                                            ? rakennus.properties.yleistiedot["Kohteen osoitteet"].value.join(", ")
+                                            : rakennus.properties?.yleistiedot["Kohteen osoitteet"].value
+                                        }{" "}
+                                        {rakennus.properties?.yleistiedot["Toimipaikka"].value}
+                                        {` (${rakennus.properties?.rakennustiedot["Rakennusluokitus"].value})`}
+                                    </span>
 
-      <Modal show={showReport} onHide={() => setShowReport(false)} size="lg">
-        <Modal.Header closeButton>
-          <Modal.Title>Muokkaa raporttia</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {reportRakennus && <EditableReport rakennus={reportRakennus} />}
-        </Modal.Body>
-      </Modal>
-    </div>
-  );
+                                    <button
+                                        className="btn btn-outline-primary btn-sm ms-2"
+                                        onClick={() => setMapCoords(rakennus.geometry?.coordinates)}
+                                        >
+                                        <i className="bi bi-geo-alt-fill me-1"></i> Näytä kartalla
+                                    </button>
+
+                                    </div> 
+
+                                    <Accordion>
+                                        <Accordion.Item eventKey="0">
+                                            <Accordion.Header>Yleistiedot</Accordion.Header>
+                                            <Accordion.Body>
+                                                <Tabletemplate
+                                                    properties={rakennus.properties.yleistiedot}
+                                                    tableTitle={''}
+                                                />
+                                            </Accordion.Body>
+                                        </Accordion.Item>
+
+                                        <Accordion.Item eventKey="1">
+                                            <Accordion.Header>Tekniset tiedot</Accordion.Header>
+                                            <Accordion.Body>
+                                                <Tabletemplate
+                                                    properties={rakennus.properties.teknisettiedot}
+                                                    tableTitle={''}
+                                                />
+                                            </Accordion.Body>
+                                        </Accordion.Item>
+
+                                        <Accordion.Item eventKey="2">
+                                            <Accordion.Header>Rakennustiedot</Accordion.Header>
+                                            <Accordion.Body>
+                                                <Tabletemplate
+                                                    properties={rakennus.properties.rakennustiedot}
+                                                    tableTitle={''}
+                                                />
+                                            </Accordion.Body>
+                                        </Accordion.Item>
+
+                                        <Accordion.Item eventKey="3">
+                                            <Accordion.Header>Aluetiedot</Accordion.Header>
+                                            <Accordion.Body>
+                                                <Tabletemplate
+                                                    properties={rakennus.properties.aluetiedot}
+                                                    tableTitle={''}
+                                                />
+                                            </Accordion.Body>
+                                        </Accordion.Item>
+                                    </Accordion>
+                                </div>
+                            </div>
+                        ))
+                    ) : (
+                        <div className="p-2">Ei rakennuksia</div>
+                    )}
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default Resultdisplay;
