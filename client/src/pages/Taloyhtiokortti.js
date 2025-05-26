7bc826f5f65a6eba751d371ab46cc36d864f7a26
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2025-05-26 16:51:13 +0300

    data tabs kiinteistokortti

--- client/src/pages/Taloyhtiokortti.js
+++ client/src/pages/Taloyhtiokortti.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import Badge from 'react-bootstrap/Badge';
 import { useParams } from 'react-router-dom';
+import MapVisual from '../components/MapVisual';
+import { Tab, Tabs } from 'react-bootstrap';
 
 function Taloyhtiokortti() {
     const { id } = useParams();
@@ -22,9 +24,41 @@ function Taloyhtiokortti() {
         <h1 className="otsikko text-primary mb-1 mx-auto">
             Kiinteistötietopankki <Badge bg="secondary">DEMO</Badge>
         </h1>
-        <div className="container mt-3">
-            <h1>{card.title}</h1>
-            <p>{card.description}</p>
+        <div className="container mt-3 border border-primary">
+
+            <div className='row border border-secondary'>
+                <div class="col-md-6 border border-primary">            
+                <h1>{card.title}</h1> 
+                <p>{card.description}</p>
+            </div>
+                <div class="col-md-6 border border-primary"><MapVisual pos={[65.00816937, 25.46030678]} coords={[65.00816937, 25.46030678]}/></div>
+            </div>
+
+            <div className='row border border-danger mt-3'>
+                <Tabs
+                    defaultActiveKey="dokumentit"
+                    id="fill-tab-example"
+                    className="mb-3"
+                    fill
+                >
+                    <Tab eventKey="dokumentit" title="Dokumentit ja raportit">
+                        Tab content for Dokumentit ja raportit
+                    </Tab>
+                    <Tab eventKey="perustiedot" title="Perustiedot">
+                        Tab content for Perustiedot
+                    </Tab>
+                    <Tab eventKey="kiinteistotiedot" title="Kiinteistotiedot">
+                        Tab content for Kiinteistotiedot
+                    </Tab>
+                    <Tab eventKey="rhtiedot" title="RH-tiedot">
+                        Tab content for RH-tiedot
+                    </Tab>
+                    <Tab eventKey="pts" title="PTS">
+                        Tab content for PTS
+                    </Tab>
+                </Tabs>
+
+            </div>
         </div>
     </>
   );
