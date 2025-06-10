c0c369e7aed12a0ce2ca99e1e24ba75adeb0d8f0
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2025-06-10 12:27:40 +0300

    tk edits

--- client/src/pages/Taloyhtiokortti.js
+++ client/src/pages/Taloyhtiokortti.js
@@ -34,10 +34,15 @@ function Taloyhtiokortti() {
 
             <div className='row border border-secondary'>
                 <div className="col-md-6 border border-primary">            
-                <h1>{card.kiinteistotunnus}</h1> 
-                <p>{card?.rakennukset[0]?.osoite}</p>
-                {/* <StickyAfterScroll></StickyAfterScroll> */}
-            </div>
+                    <h2>{card?.rakennukset[0]?.osoite} {card?.rakennukset[0]?.toimipaikka}</h2>
+                    <h4>{card.kiinteistotunnus}</h4> 
+                    {/* {card?.rakennukset.map(rakennus => (
+                    <p key={rakennus.id_rakennus}>
+                        {rakennus.rakennustunnus}
+                    </p>) )} */}
+                    
+                
+                </div>
                 <div className="col-md-6 border border-primary"><MapVisual pos={[65.00816937, 25.46030678]} coords={[card?.rakennukset[0]?.rakennustiedot[0]?.sijainti?.coordinates[1],card?.rakennukset[0]?.rakennustiedot[0]?.sijainti?.coordinates[0]]}/></div>
             </div>
 
@@ -52,7 +57,70 @@ function Taloyhtiokortti() {
                         Tab content for Dokumentit ja raportit
                     </Tab>
                     <Tab eventKey="perustiedot" title="Perustiedot">
-                        Tab content for Perustiedot
+                        <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Tunnus</th>
+                                        <th scope="col">Rakennusvuosi</th>
+                                        <th scope="col">Kerroksia</th>
+                                        <th scope="col">Kokonaisala m²</th>
+                                        <th scope="col">Kerrosala m²</th>
+                                        <th scope="col">Huoneistoala m²</th>
+                                        <th scope="col">Tilavuus m³</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {(() => {
+                                        let totalKerroksia = 0;
+                                        let totalKokonaisala = 0;
+                                        let totalKerrosala = 0;
+                                        let totalHuoneistoala = 0;
+                                        let totalTilavuus = 0;
+
+                                        const rows = card?.rakennukset.map(rakennus => {
+                                            const tiedot = rakennus.rakennustiedot?.[0] || {};
+                                            const kerroksia = Number(tiedot.kerroksia) || 0;
+                                            const kokonaisala = Number(tiedot.kokonaisala) || 0;
+                                            const kerrosala = Number(tiedot.kerrosala) || 0;
+                                            const huoneistoala = Number(tiedot.huoneistoala) || 0;
+                                            const tilavuus = Number(tiedot.tilavuus) || 0;
+
+                                            totalKerroksia += kerroksia;
+                                            totalKokonaisala += kokonaisala;
+                                            totalKerrosala += kerrosala;
+                                            totalHuoneistoala += huoneistoala;
+                                            totalTilavuus += tilavuus;
+
+                                            return (
+                                                <tr key={rakennus.id_rakennus}>
+                                                    <th scope="row">{rakennus.rakennustunnus}</th>
+                                                    <td>{tiedot.rakennusvuosi || 'Ei tiedossa'}</td>
+                                                    <td>{kerroksia}</td>
+                                                    <td>{kokonaisala}</td>
+                                                    <td>{kerrosala}</td>
+                                                    <td>{huoneistoala}</td>
+                                                    <td>{tilavuus}</td>
+                                                </tr>
+                                            );
+                                        });
+
+                                        return (
+                                            <>
+                                                {rows}
+                                                <tr>
+                                                    <th scope="row">Yhteensä</th>
+                                                    <td>-</td>
+                                                    <td>-</td>
+                                                    <td>{totalKokonaisala}</td>
+                                                    <td>{totalKerrosala}</td>
+                                                    <td>{totalHuoneistoala}</td>
+                                                    <td>{totalTilavuus}</td>
+                                                </tr>
+                                            </>
+                                        );
+                                    })()}
+                                </tbody>
+                            </table>
                     </Tab>
                     <Tab eventKey="kiinteistotiedot" title="Kiinteistotiedot">
                         Tab content for Kiinteistotiedot
