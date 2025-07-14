63bfd646b6c26dbb79f5c9bb89ef98d74db261d4
Joonas <joonasroska123@gmail.com>
2025-07-14 14:30:30 +0300

    tuloste change

--- client/src/api/mmlTulosteet.js
+++ client/src/api/mmlTulosteet.js
@@ -4,19 +4,19 @@ const API_URL = process.env.REACT_APP_API_URL;
 
 
 
-export const getRasitustodistus = (ktunnus, withPersonalData = true) =>
+export const getRasitustodistus = (ktunnus, withPersonalData = false) =>
   axios.get(`${API_URL}/api/mmltulosteet/rasitustodistus/${withPersonalData ? 'henkilotiedoilla' : 'ilmanhenkilotietoja'}/${ktunnus}`, {
     responseType: 'blob',
     withCredentials: true,
   });
 
-export const getLainhuutotodistus = (ktunnus, withPersonalData = true) =>
+export const getLainhuutotodistus = (ktunnus, withPersonalData = false) =>
   axios.get(`${API_URL}/api/mmltulosteet/lainhuutotodistus/${withPersonalData ? 'henkilotiedoilla' : 'ilmanhenkilotietoja'}/${ktunnus}`, {
     responseType: 'blob',
     withCredentials: true,
   });
 
-export const getVuokraoikeustodistus = (ktunnus, withPersonalData = true) =>
+export const getVuokraoikeustodistus = (ktunnus, withPersonalData = false) =>
   axios.get(`${API_URL}/api/mmltulosteet/vuokraoikeustodistus/${withPersonalData ? 'henkilotiedoilla' : 'ilmanhenkilotietoja'}/${ktunnus}`, {
     responseType: 'blob',
     withCredentials: true,
