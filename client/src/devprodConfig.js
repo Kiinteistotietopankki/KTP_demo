e8684183e209e12f92f218d34889ed546faf26ea
Joonas <joonasroska123@gmail.com>
2025-08-01 16:57:52 +0300

    config

--- client/src/devprodConfig.js
+++ client/src/devprodConfig.js
@@ -1,9 +1,11 @@
 const isProd = process.env.REACT_APP_ENVIRONMENT;
+const produrl = process.env.REACT_APP_PROD_API_URL
+const devurl = process.env.REACT_APP_DEV_API_URL
 
 const config = {
   apiBaseUrl: isProd
-    ? process.env.REACT_APP_PROD_API_URL
-    : process.env.REACT_APP_DEV_API_URL,
+    ? produrl
+    : devurl,
 
   mapTileTemplate: '/api/kartat/fetch-tile/{layerName}/{tileMatrixSet}/{z}/{y}/{x}'
 };
